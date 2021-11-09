5addafd8f721f9f87392373ca77aa906eb06c943
Rishabh Singh <360rishabhsingh.cs@gmail.com>
2021-11-09 13:23:43 -0500

    HomeWork for GeekSter

---
+++ Intro.java
@@ -0,0 +1,61 @@
+//Homework
+
+public class Intro {
+
+    public static void main(String args[]){
+
+//    Using While Loop
+
+//    Print (1 to 10) using while loop......
+
+        System.out.println("Print (1 to 10) using while loop......");
+        int i = 1;
+        while (i <= 10){
+            System.out.println(i);
+            i++;
+        }
+
+//    Print (10 to 1) Using while loop.......
+
+        System.out.println("Print (10 to 1) Using while loop.......");
+        int j = 10;
+        while(j >= 1){
+            System.out.println(j);
+            j--;
+        }
+
+//    Print 2's table using while loop........
+
+        System.out.println("Print 2's table using while loop........");
+        int a = 2;
+        while (a <= 20){
+            System.out.println(a);
+            a = a + 2;
+        }
+
+//    Print 2's table using while loop in Reverse Order........
+
+        System.out.println("Print 2's table using while loop in Reverse Order........");
+        int b = 20;
+        while (b >= 2){
+            System.out.println(b);
+            b = b - 2;
+        }
+
+//    Print 2's table using for loop........
+
+        System.out.println("Print 2's table using for loop........");
+        for(int c = 2; c<=20; c=c+2){
+            System.out.println(c);
+        }
+
+//    Print 2's table using for loop in reverse order........
+
+        System.out.println("Print 2's table using for loop in reverse order........");
+        for(int z=20; z>=2; z=z-2){
+            System.out.println(z);
+        }
+
+        }
+    }
+
