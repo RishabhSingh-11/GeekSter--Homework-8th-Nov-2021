7f595488388703669b73a93e8f7ecfc539ac9468
RishabhSingh-11 <84684033+RishabhSingh-11@users.noreply.github.com>
2021-11-14 02:27:39 +0530

    HomeWork of 10th November

---
+++ DataTypes.java
@@ -0,0 +1,57 @@
+// Assignment of Day-3 (10/11/2021) class taken by Jay Sir !
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        // how to declear byte datatype :
+
+        byte b = 1;
+        System.out.println("The byte value is : " + b);
+
+        // how to declear short value :
+
+        short a = 155;
+        System.out.println("The short value is : " + a);
+
+        // how to declear integer value :
+
+        int x = 11092021;
+        System.out.println("The integer value is : " + x);
+
+        // how to declear long value :
+
+        long l = 1109202111;
+        System.out.println("The long value is : " + l);
+
+        // how to declear float datatype :
+
+        float f = 125.5f;
+        System.out.println("The float value is : " + f);
+
+        // how to declear double datatype :
+
+        double d = 125.111555666;
+        System.out.println("The double value is : " + d);
+
+        // how to declear char datatype :
+
+        char ch = 'R';
+        System.out.println("The character value is : " + ch);
+
+        // how to declear String datatype :
+
+        String str = "Rishbah Singh";
+        System.out.println("The String value is : " + str);
+
+        // how to declear boolean datatype :
+
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println("The boolean value is : " + b1);
+        System.out.println("The boolean value is : " + b2);
+
+
+    }
+
+}
